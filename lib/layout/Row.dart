611beac2bfe4d13aa29f6caea60246b20352b3d7
xieguo <xieguo2007@126.com>
2019-10-29 23:59:31 +0800

    Add row and column example

---
+++ lib/layout/Row.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(
+    MaterialApp(
+        title: '水平布局示例',
+        home: LayoutDemo(),
+    ),
+);
+
+class LayoutDemo extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('水平布局示例'),
+            ),
+            body: Row(
+                children: <Widget>[
+                    Expanded(
+                        child: Text(
+                            '左侧文本',
+                            textAlign: TextAlign.center,
+                        ),
+                    ),
+                    Expanded(
+                        child: Text(
+                            '中间文本',
+                            textAlign: TextAlign.center,
+                        ),
+                    ),
+                    Expanded(
+                        child: FittedBox(
+                            fit: BoxFit.contain,
+                            child: const FlutterLogo(),
+                        ),
+                    ),
+                ],
+            ),
+        );
+    }
+}
